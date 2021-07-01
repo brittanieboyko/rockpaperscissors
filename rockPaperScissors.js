07e7169ae8fd8f7cd78a4808319b39cf3e90ec7e
Brittanie <brittanie.boyko@gmail.com>
2021-07-01 09:25:04 -0400

    created function to test user selection vs computer selection and determine winner

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,9 +1,27 @@
-console.log("hello!");
-
 function computerPlay() {
-    const playOptions = ["Rock", "Paper", "Scissors"];
+    const playOptions = ["rock", "paper", "scissors"];
     const randomComputerSelection = playOptions[Math.floor(Math.random() * playOptions.length)];
     return randomComputerSelection;
 }
 
-console.log(computerPlay());
+function playRound(userSelection, computerSelection) {
+
+    const formattedUserSelection = userSelection.toLowerCase();
+    console.log(formattedUserSelection, computerSelection);
+    if (formattedUserSelection === computerSelection) {
+        return "draw!";
+    } else if (formattedUserSelection === "rock" && computerSelection === "scissors") {
+        return "User wins! Rock beats scissors.";
+    } else if (formattedUserSelection === "rock" && computerSelection === "paper") {
+        return "Computer wins! Paper beats rock.";
+    } else if (formattedUserSelection === "paper" && computerSelection === "rock") {
+        return "User wins! Paper beats rock.";
+    } else if (formattedUserSelection === "paper" && computerSelection === "scissors") {
+        return "Computer wins! Scissors beat paper.";
+    } else if (formattedUserSelection === "scissors" && computerSelection === "rock") {
+        return "Computer wins! Rock beats paper.";
+    } else if (formattedUserSelection === "scissors" && computerSelection === "paper") {
+        return "User wins! Scissors beat paper.";
+    }
+}
+console.log(playRound("Rock", computerPlay()));
