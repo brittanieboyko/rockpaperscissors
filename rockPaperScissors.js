7fdbe4f9705f64b3004071ac605b5b0b2b0f813e
Brittanie <brittanie.boyko@gmail.com>
2021-07-01 09:01:59 -0400

    created function to generate random choice of rock, paper, or scissor

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,9 @@
 console.log("hello!");
+
+function computerPlay() {
+    const playOptions = ["Rock", "Paper", "Scissors"];
+    const randomComputerSelection = playOptions[Math.floor(Math.random() * playOptions.length)];
+    return randomComputerSelection;
+}
+
+console.log(computerPlay());
